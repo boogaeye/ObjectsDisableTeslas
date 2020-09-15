6e7987753fcd400102a0c6011f6e07657ba5bd41
boogaeye <bradleigh.christensen@makt3k.com>
2020-09-14 20:18:23 -0700

    Add project files.

---
+++ ObjectsDisableTeslas/ObjectsDisableTeslas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+
+namespace ObjectsDisableTeslas
+{
+    public class ObjectsDisableTeslas
+    {
+
+    }
+}

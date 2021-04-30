b2fb2542083f0ad90417d1c0129a31a5ad529409
boogaeye <bradleigh.christensen@makt3k.com>
2021-04-29 19:27:55 -0700

    Add files via upload

---
+++ ObjectsDisableTeslas/ObjectsDisableTeslas/Config.cs
@@ -0,0 +1,37 @@
+﻿namespace ObjectsDisableTeslas
+{
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Exiled.API.Interfaces;
+
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public int HoldTime { get; set; } = 5;
+
+        [Description("Disables the tesla gate for the Hold Time")]
+        public bool HoldTesla { get; set; } = false;
+
+        [Description("the level scp207 has to be to be immune to tesla gates. set it to -1 to disable")]
+        public int Scp207LevelTeslaImmune { get; set; } = -1;
+
+        [Description("makes it where if your invisable you cant trigger teslas")]
+        public bool EnableScp268TeslaDisable { get; set; } = false;
+
+        [Description("sets certain roles that wont trigger tesla gates however personally I recommend leaving this at its default value")]
+        public RoleType[] DisableTeslaRoles { get; set; } = { };
+
+        [Description("Item thats allowed to disable teslas : if set true then Allow to be nonholdable")]
+        public Dictionary<ItemType, bool> TeslaDisableItems { get; set; } = new Dictionary<ItemType, bool>() { { ItemType.KeycardO5, true }, { ItemType.KeycardFacilityManager, true }, { ItemType.KeycardContainmentEngineer, false }, { ItemType.KeycardNTFCommander, false }, { ItemType.KeycardNTFLieutenant, false }, { ItemType.KeycardSeniorGuard, false } };
+
+        [Description("sets when you take out the items what the hint will say")]
+        public string[] TeslaHints { get; set; } = { "This Item Will Disable tesla gates when held", "This Item Will not trigger tesla gates as long as you hold it" };
+
+        [Description("sets when you take out the items what the hint will say")]
+        public string[] TeslaHintsNotHoldable { get; set; } = { "This Item Will Disable tesla gates without needing to hold it", "This Item Will not trigger tesla gates as long as its in your inventory" };
+
+        [Description("sets the amount of time it will say the hints")]
+        public int TeslaHintDuration { get; set; } = 5;
+    }
+}
---
+++ ObjectsDisableTeslas/ObjectsDisableTeslas/EventHandlers.cs
@@ -0,0 +1,129 @@
+﻿namespace ObjectsDisableTeslas
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using CustomPlayerEffects;
+    using Exiled.API.Features;
+    using Exiled.Events.EventArgs;
+    using MEC;
+
+    public class EventHandlers
+    {
+        private readonly Plugin<Config> plugin;
+
+        public EventHandlers(Plugin<Config> plugin) => this.plugin = plugin;
+
+        private readonly Random random = new Random();
+
+        public List<string> teslaLocks = new List<string>();
+
+        public void SwitchItem(ChangingItemEventArgs ev)
+        {
+            if (this.plugin.Config.TeslaDisableItems.ContainsKey(ev.NewItem.id))
+            {
+                if (this.plugin.Config.TeslaDisableItems[ev.NewItem.id])
+                {
+                    if (this.plugin.Config.TeslaHintsNotHoldable.Length == 0)
+                    {
+                        return;
+                    }
+
+                    ev.Player.ShowHint(
+                        this.plugin.Config.TeslaHintsNotHoldable[
+                            random.Next(0, this.plugin.Config.TeslaHintsNotHoldable.Length)],
+                        this.plugin.Config.TeslaHintDuration);
+                }
+                else
+                {
+                    if (this.plugin.Config.TeslaHints.Count() == 0)
+                        return;
+
+                    ev.Player.ShowHint(
+                        this.plugin.Config.TeslaHints[random.Next(0, this.plugin.Config.TeslaHints.Length)],
+                        this.plugin.Config.TeslaHintDuration);
+                }
+            }
+        }
+
+        public void OnTeslaTrigger(TriggeringTeslaEventArgs ev)
+        {
+            string teslaName = ev.Player.CurrentRoom.Name;
+            if (teslaLocks.Contains(teslaName))
+            {
+                ev.IsTriggerable = false;
+                return;
+            }
+
+            if (ev.Player.GetEffectActive<Scp268>() && this.plugin.Config.EnableScp268TeslaDisable)
+            {
+                ev.IsTriggerable = false;
+                return;
+            }
+
+            if (this.plugin.Config.DisableTeslaRoles.Contains(ev.Player.Role))
+            {
+                ev.IsTriggerable = false;
+
+                Timing.RunCoroutine(Swiping(teslaName));
+
+                return;
+            }
+
+            foreach (Inventory.SyncItemInfo i in ev.Player.Inventory.items)
+            {
+                if (this.plugin.Config.TeslaDisableItems.ContainsKey(i.id))
+                {
+                    if (this.plugin.Config.TeslaDisableItems.TryGetValue(i.id, out bool value) && value)
+                    {
+                        ev.IsTriggerable = false;
+                        Timing.RunCoroutine(Swiping(teslaName));
+                        break;
+                    }
+
+                    if (this.plugin.Config.TeslaDisableItems.ContainsKey(ev.Player.Inventory.curItem))
+                    {
+                        ev.IsTriggerable = false;
+                        Timing.RunCoroutine(Swiping(teslaName));
+                        break;
+                    }
+
+                    ev.IsTriggerable = true;
+                }
+                else
+                {
+                    ev.IsTriggerable = true;
+                }
+            }
+        }
+
+        public IEnumerator<float> Swiping(string name)
+        {
+            if (this.plugin.Config.HoldTesla)
+            {
+                teslaLocks.Add(name);
+                yield return Timing.WaitForSeconds(this.plugin.Config.HoldTime);
+                teslaLocks.Remove(name);
+            }
+        }
+
+        public void OnHurt(HurtingEventArgs ev)
+        {
+            if (this.plugin.Config.Scp207LevelTeslaImmune != -1 && this.plugin.Config.Scp207LevelTeslaImmune <= ev.Target.GetEffectIntensity<Scp207>())
+            {
+                if (ev.HitInformations.GetDamageType() == DamageTypes.Tesla)
+                {
+                    ev.IsAllowed = false;
+                }
+            }
+        }
+
+        public void HealItem(UsingMedicalItemEventArgs ev)
+        {
+            if (this.plugin.Config.Scp207LevelTeslaImmune != -1 && this.plugin.Config.Scp207LevelTeslaImmune - 1 <= ev.Player.GetEffectIntensity<Scp207>() && ev.Item == ItemType.SCP207)
+            {
+                ev.Player.ShowHint("You are now immune to tesla gates");
+            }
+        }
+    }
+}
